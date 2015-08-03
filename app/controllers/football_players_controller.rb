f7cc6ea6a5c91e1d699ac1cc2af2d3a9b1c23927
Pavan Katepalli <pavankat@gmail.com>
2015-08-03 10:17:50 -0400

    jsonifying controllers get rid of new edit

--- app/controllers/football_players_controller.rb
+++ app/controllers/football_players_controller.rb
@@ -1,64 +1,35 @@
 class FootballPlayersController < ApplicationController
-  before_action :set_football_player, only: [:show, :edit, :update, :destroy]
+  before_action :set_football_player, only: [:show, :update, :destroy]
 
-  # GET /football_players
-  # GET /football_players.json
   def index
-    @football_players = FootballPlayer.all
+    render json: FootballPlayer.all
   end
 
-  # GET /football_players/1
-  # GET /football_players/1.json
   def show
+    render json: @football_player
   end
 
-  # GET /football_players/new
-  def new
-    @football_player = FootballPlayer.new
-  end
-
-  # GET /football_players/1/edit
-  def edit
-  end
-
-  # POST /football_players
-  # POST /football_players.json
   def create
-    @football_player = FootballPlayer.new(football_player_params)
+    football_player = FootballPlayer.new(football_player_params)
 
-    respond_to do |format|
-      if @football_player.save
-        format.html { redirect_to @football_player, notice: 'Football player was successfully created.' }
-        format.json { render :show, status: :created, location: @football_player }
-      else
-        format.html { render :new }
-        format.json { render json: @football_player.errors, status: :unprocessable_entity }
-      end
+    if football_player.save
+      render json: football_player 
+    else
+      render json: @football_player.errors, status: :unprocessable_entity #uses 422 for the status code - http://guides.rubyonrails.org/layouts_and_rendering.html
     end
   end
 
-  # PATCH/PUT /football_players/1
-  # PATCH/PUT /football_players/1.json
   def update
-    respond_to do |format|
-      if @football_player.update(football_player_params)
-        format.html { redirect_to @football_player, notice: 'Football player was successfully updated.' }
-        format.json { render :show, status: :ok, location: @football_player }
-      else
-        format.html { render :edit }
-        format.json { render json: @football_player.errors, status: :unprocessable_entity }
-      end
+    if @football_player.update(football_player_params)
+      render json: @football_player
+    else
+      render json: @football_player.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /football_players/1
-  # DELETE /football_players/1.json
   def destroy
     @football_player.destroy
-    respond_to do |format|
-      format.html { redirect_to football_players_url, notice: 'Football player was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render json: @football_player
   end
 
   private
